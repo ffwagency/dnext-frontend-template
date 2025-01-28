3c7e1ce271a205f131321d303e73097593f933e2
kristiyan.velkov <kristiyan.velkov@ffw.com>
2025-01-28 11:59:48 +0200

    [feat] added 404 page

--- app/[lang]/error.tsx
+++ app/[lang]/error.tsx
@@ -14,20 +14,24 @@ export default function Error({
     }, [error]);
 
     return (
-        <div className="bg-gray-100 my-9 flex flex-col items-center justify-center text-center">
-            <div className="notfound-404 mb-8">
-                <h1 className="text-gray-800 text-6xl font-bold">
-                    Something went wrong!
-                </h1>
+        <section className="bg-white dark:bg-gray-900">
+            <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
+                <div className="max-w-screen-sm mx-auto text-center">
+                    <h1 className="text-blue-600 dark:text-blue-500 mb-4 text-4xl font-extrabold tracking-tight lg:text-9xl">
+                        Error
+                    </h1>
+                    <p className="text-gray-900 dark:text-white mb-4 text-2xl font-bold tracking-tight md:text-4xl">
+                        {error.message}
+                    </p>
+                    <button
+                        type="button"
+                        className="text-white bg-red-600 hover:bg-blue-800 focus:ring-blue-300 dark:focus:ring-blue-900 my-4 inline-flex rounded-lg px-5 py-2.5 text-center text-sm font-medium focus:outline-none focus:ring-4"
+                        onClick={reset}
+                    >
+                        Try again
+                    </button>
+                </div>
             </div>
-            <p className="text-gray-600 mt-4">{error.message}</p>
-            <button
-                type="button"
-                className="menu-link button--primary mx-auto mt-5 block border-b border-b-transparent px-2 py-1 text-lg font-bold text-light hover:border-b-primary"
-                onClick={reset}
-            >
-                Try again
-            </button>
-        </div>
+        </section>
     );
 }
--- app/[lang]/not-found.tsx
+++ app/[lang]/not-found.tsx
@@ -1,54 +1,28 @@
-import { cookies } from "next/headers";
-import Image from "next/image";
 import Link from "next/link";
-import sanitizeHtml from "sanitize-html";
-
-import { LANGUAGES } from "@/config";
-import { getNotFoundPage } from "@/lib";
 
 async function NotFoundPage(): Promise<JSX.Element | undefined> {
-    const localeCookie =
-        (await cookies()).get("NEXT_LOCALE")?.value ?? LANGUAGES.defaultLocale;
-    const data = await getNotFoundPage(localeCookie);
-
     return (
-        <section className="bg-gray-100 my-9 flex flex-col items-center justify-center text-center">
-            {data && data?.pictureUrl && (
-                <Image
-                    className="text-center"
-                    loading="eager"
-                    src={data.pictureUrl}
-                    width="400"
-                    height="400"
-                    alt="Not found image"
-                    priority
-                    sizes="(max-width: 425px) 20vw,(max-width: 768px) 40vw, (max-width: 1200px) 30w, 33vw"
-                    placeholder="blur"
-                    blurDataURL="./default-blur.webp"
-                />
-            )}
-
-            <div className="mb-8">
-                <h1 className="text-gray-800 text-6xl font-bold">
-                    {data?.title}
-                </h1>
+        <section className="bg-white dark:bg-gray-900">
+            <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
+                <div className="max-w-screen-sm mx-auto text-center">
+                    <h1 className="text-blue-600 dark:text-blue-500 mb-4 text-7xl font-extrabold tracking-tight lg:text-9xl">
+                        404
+                    </h1>
+                    <p className="text-gray-900 dark:text-white mb-4 text-3xl font-bold tracking-tight md:text-4xl">
+                        Something's missing.
+                    </p>
+                    <p className="text-gray-500 dark:text-gray-400 mb-4 text-lg font-light">
+                        Sorry, we can't find that page. You'll find lots to
+                        explore on the home page.{" "}
+                    </p>
+                    <Link
+                        href="/"
+                        className="text-white bg-blue-600 hover:bg-blue-800 focus:ring-blue-300 dark:focus:ring-blue-900 my-4 inline-flex rounded-lg px-5 py-2.5 text-center text-sm font-medium focus:outline-none focus:ring-4"
+                    >
+                        Back to Homepage
+                    </Link>
+                </div>
             </div>
-            {data && data.text && (
-                <p
-                    className="text-gray-600 mt-4"
-                    dangerouslySetInnerHTML={{
-                        __html: sanitizeHtml(data.text),
-                    }}
-                />
-            )}
-            {data && data.linkUrl && (
-                <Link
-                    href={data?.linkUrl}
-                    className="menu-link button--primary mx-auto mt-5 block border-b border-b-transparent px-2 py-1 text-lg font-bold text-light hover:border-b-primary"
-                >
-                    {data?.linkText}
-                </Link>
-            )}
         </section>
     );
 }
